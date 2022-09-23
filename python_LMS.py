33b0a0095a301aca03edd6e7e4f5a3d8a2a99ccc
jbernalg <jeinffersonbernal@gmail.com>
2022-09-22 21:39:51 -0500

    lms 1.2

--- python_LMS.py
+++ python_LMS.py
@@ -2,4 +2,26 @@
 import datetime
 import os
 
-os.getcwd()
+# os.getcwd() #muestra la carpeta donde se encuentra el archivo
+
+class LMS:
+    """this class is used to keep record of book library
+    It has total four module: - Display books
+                              - Issue books
+                              - Return books
+                              - Add books"""
+    def __init__(self, list_of_books, library_name):
+        self.list_of_books = 'List_of_books.txt'
+        self.library_name = library_name
+        self.books_dict = {}  #contiene toda la informacion de cada libro
+        Id = 101  #identificador
+
+        #leemos el archivo txt
+        with open(self.list_of_books) as bk:
+            content = bk.readlines()  #almacena las lineas del archivo
+
+        for line in content:
+            print(line)
+
+print(LMS("list_of_books.txt", "Python's library"))
+
