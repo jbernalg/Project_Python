d8efa1d7f4feb644360178d80a8d5370a210c175
jbernalg <jeinffersonbernal@gmail.com>
2022-09-23 13:12:35 -0500

    lms 1.7

--- python_LMS.py
+++ python_LMS.py
@@ -47,7 +47,15 @@ class LMS:
         current_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         #verificamos si el ID existe o no en la BD
-        
+        if books_id in self.books_dict.keys():
+            if not self.books_dict[books_id]['Status'] == 'Available':
+                print(f"This books is already issued to {self.books_dict[books_id]['lender_name']} \
+                    on {self.books_dict[books_id]['Issue_data']}")
+                return self.Issue_books()
+            elif self.books_dict[books_id]['Status'] == 'Available':
+                your_name = input('Enter your name: ')
+                self.books_dict[books_id]['lender_name'] == your_name
+                self.books_dict[books_id]['Issue_date'] == current_date
 
 
 l = LMS("List_of_books.txt", "Python's Library")
