ac507994227850aa851631c7097e9d66e101ccdd
jbernalg <jeinffersonbernal@gmail.com>
2022-09-26 15:28:38 -0500

    downloader 1.2

--- Video_Downloader/downloader.py
+++ Video_Downloader/downloader.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from tkinter import *
 from tkinter import filedialog
 
@@ -5,5 +6,13 @@ screen = Tk()  #pantalla de la app
 title = screen.title('Descarga Videos de Youtube') # titulo de la app
 canvas = Canvas(screen, width=500, height=500)  # medidas del lienzo de la app
 canvas.pack()
+
+#logo
+logo_img = PhotoImage(file='logo_youtube.png')
+#redimensionar image
+logo_img = logo_img.subsample(2, 2)
+
+#agregar la imagen al lienzo
+canvas.create_image(250, 80, image=logo_img)
 
 screen.mainloop()
