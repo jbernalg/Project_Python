78d40fbacf068e76d80ee8254bf40aea6d775107
jbernalg <jeinffersonbernal@gmail.com>
2022-09-26 15:54:34 -0500

    downloader 1.5

--- Video_Downloader/downloader.py
+++ Video_Downloader/downloader.py
@@ -1,4 +1,5 @@
 from email.mime import image
+from select import select
 from tkinter import *
 from tkinter import filedialog
 
@@ -19,13 +20,20 @@ canvas.create_image(250, 80, image=logo_img)
 link_field = Entry(screen, width=50)
 link_label = Label(screen, text='Ingresa enlace de Descarga: ', font=('Arial', 15))
 
-
+#seleccione ruta donde guarda el video
+path_label = Label(screen, text='Seleccionar Ruta para Descargar', font=('Arial', 15))
+select_btn = Button(screen, text='Seleccionar')
 
 #añadir widgets a la ventana
 canvas.create_window(250, 190, window=link_label)
 canvas.create_window(250, 220, window=link_field)
+canvas.create_window(250, 280,window=path_label)
+canvas.create_window(250, 330,window=select_btn)
 
 #boton de descarga
 download_btn = Button(screen, text='Download File')
+
+#añadir boton a la ventana
+canvas.create_window(250, 390,window=download_btn)
 
 screen.mainloop()
