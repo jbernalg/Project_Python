ca05ebf8e80824b78e3b0f61a9ca2b3b46c4769f
jbernalg <jeinffersonbernal@gmail.com>
2022-12-15 09:06:13 -0500

    gui 1.2

--- GUI_Modern/main.py
+++ GUI_Modern/main.py
@@ -19,5 +19,30 @@ def login():
 # frame donde iran los botones
 frame = customtkinter.CTkFrame(master=root)
 
-# configuracion del frame
+# relleno del frame
 frame.pack(pady=20, padx=60, fill='both', expand=True)
+
+# agregar una etiqueta al frame
+label = customtkinter.CTkLabel(master=frame, 
+                               text='Inicio de Sesion', font=('Roboto', 24))
+
+# relleno de la etiqueta
+label.pack(pady=12, padx=10)
+
+# entradas de texto
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text='Username')
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text='Password', show='*')
+entry2.pack(pady=12, padx=10)
+
+# boton inicio de sesion
+button = customtkinter.CTkButton(master=frame, text='Login', command=login)
+button.pack(pady=12, padx=10)
+
+# casilla de verificacion
+checkbox = customtkinter.CTkCheckBox(master=frame, text='Recordarme')
+checkbox.pack(pady=12, padx=10)
+
+# bucle del punto raiz
+root.mainloop()
