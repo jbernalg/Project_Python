4f714c562719b0ad864f1e9888bb0be1d1d660d2
jbernalg <jeinffersonbernal@gmail.com>
2022-12-16 11:17:49 -0500

    kivy lab 1.2

---
+++ Kivy_Lab/main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+
+# diseño de caja para la app
+class BoxLayoutExample(BoxLayout):
+    pass
+
+# interfaz principal de la app
+class MainWidget(Widget):
+    pass
+
+
+# clase que inicia la app
+class TheLabApp(App):
+    pass
+
+
+# linea para correr la app
+TheLabApp().run()
