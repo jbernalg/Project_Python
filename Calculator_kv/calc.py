c6ce27bb68750ae55e20f08aaa2b76172c5f9b14
jbernalg <jeinffersonbernal@gmail.com>
2022-12-28 11:57:51 -0500

    add button and textinput

--- Calculator_kv/calc.py
+++ Calculator_kv/calc.py
@@ -4,10 +4,14 @@ from kivy.properties import ObjectProperty
 from kivy.core.window import Window # configura el tamaño de la app
 
 # configurar tamaño de la app
-Window.size = (500, 700)
+Window.size = (400, 600)
 
 # clase de la app
 class calculatorApp(App):
+    pass
+
+# clase con el diseño de la app
+class mylayout(Widget):
     pass
 
 if __name__ == '__main__':
